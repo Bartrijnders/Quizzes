d1f307e212578238eca93842eb6ddd4bb256c99b
Bart Rijnders <b.rijnders@student.fontys.nl>
2019-11-14 11:52:33 +0100

    added configration files

---
+++ Presentation/src/com/company/HomeScreen.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import javafx.scene.Scene;
+
+public class HomeScreen {
+
+private Scene scene;
+private final int width;
+private final int height;
+
+    public HomeScreen(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+}
